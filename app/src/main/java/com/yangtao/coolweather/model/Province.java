d74ced56f9a0b7ede36daef4ba7fac82eff73fbd
Yangtao <yangtao17@foxmail.com>
2016-05-19 17:18:42 +0800

    新建项目

---
+++ app/src/main/java/com/yangtao/coolweather/model/Province.java
@@ -0,0 +1,34 @@
+package com.yangtao.coolweather.model;
+
+/**
+ * Created by YANGTAO on 2016/5/19.
+ */
+public class Province {
+    private int id;
+    private  String provinceName;
+    private  String provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(String provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+}
