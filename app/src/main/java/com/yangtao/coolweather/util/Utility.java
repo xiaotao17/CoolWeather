51b79983d4918dad7d8ccbd969431207f921080a
YangTao <yangtao17@foxmail.com>
2016-05-19 22:49:48 +0800

    加入显示天气功能

--- app/src/main/java/com/yangtao/coolweather/util/Utility.java
+++ app/src/main/java/com/yangtao/coolweather/util/Utility.java
@@ -1,5 +1,8 @@
 package com.yangtao.coolweather.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 
 import com.yangtao.coolweather.db.CoolWeatherDB;
@@ -7,6 +10,12 @@ import com.yangtao.coolweather.model.City;
 import com.yangtao.coolweather.model.County;
 import com.yangtao.coolweather.model.Province;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 
 /**
@@ -70,5 +79,38 @@ public class Utility {
             }
         }
         return false;
+    }
+
+    /*解析服务器返回的JSON数据，并存储到本地*/
+    public static void handleWeatherResponse(Context context, String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            JSONObject weatherInfo = jsonObject.getJSONObject("weatherinfo");
+            String cityName = weatherInfo.getString("city");
+            String weatherCode = weatherInfo.getString("cityid");
+            String temp1 = weatherInfo.getString("temp1");
+            String temp2 = weatherInfo.getString("temp2");
+            String weatherDesp = weatherInfo.getString("weather");
+            String publishTime = weatherInfo.getString("ptime");
+            saveWeatherInfo(context, cityName, weatherCode, temp1, temp2, weatherDesp, publishTime);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /*将天气信息存储到SharedPreferences文件中*/
+    public static void saveWeatherInfo(Context context, String cityName,String weatherCode, String temp1,
+                                       String temp2,String weatherDesp,String publishTime) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年M月d日", Locale.CHINA);
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(context).edit();
+        editor.putBoolean("city_selected",true);
+        editor.putString("city_name",cityName);
+        editor.putString("weather_code",weatherCode);
+        editor.putString("temp1",temp1);
+        editor.putString("temp2",temp2);
+        editor.putString("weather_desp",weatherDesp);
+        editor.putString("publish_time",publishTime);
+        editor.putString("current_date",sdf.format(new Date()));
+        editor.commit();
     }
 }
